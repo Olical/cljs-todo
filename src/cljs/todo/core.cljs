c2210597ce5f37f4f7b16517d6f8ebfd9ba24f54
Oliver Caldwell <olliec87@gmail.com>
2017-09-10 12:18:15 +0100

    Allow entry of todos if not empty

--- src/cljs/todo/core.cljs
+++ src/cljs/todo/core.cljs
@@ -2,28 +2,22 @@
   (:require [reagent.core :as reagent]
             [bonsai.core :as bonsai]))
 
-;; Alright, I'll need to get data and actions correct first.
-;; Then wire things up to those actions with some rendering.
-;; Stuff you can do:
-;;  * Create a todo.
-;;  * Mark todos as done.
-;;  * Mark all todos as done.
-;;  * Delete a todo.
-;;  * Delete all done.
-;;  * Filter todos.
-
 ;; Actions.
 
 (defn create-todo
-  "Creates a todo and appends it to the list."
-  [state content]
-  (update state :todos conj {:done false
-                             :content content}))
+  "Creates a todo from the draft content and appends it to the list."
+  [{:keys [draft-todo-content] :as state}]
+  (if (empty? draft-todo-content)
+    state
+    (-> state
+        (update :todos conj {:done? false
+                             :content draft-todo-content})
+        (assoc :draft-todo-content ""))))
 
 (defn toggle-todo-done
   "Toggles the done state of the todo at the given index."
   [state index]
-  (update-in state [:todos index :done] not))
+  (update-in state [:todos index :done?] not))
 
 (defn toggle-all-todos-done
   "Toggles all todo done states. If everything is done, it marks them as not
@@ -31,8 +25,8 @@
   [state]
   (update state :todos
           (fn [todos]
-            (let [desired-state (not (every? :done todos))]
-              (vec (map #(assoc % :done desired-state) todos))))))
+            (let [desired-state (not (every? :done? todos))]
+              (vec (map #(assoc % :done? desired-state) todos))))))
 
 (defn delete-todo
   "Delete the todo at the given index."
@@ -41,18 +35,22 @@
           (fn [todos]
             (vec (concat (subvec todos 0 index) (subvec todos (inc index)))))))
 
-
 (defn delete-all-done-todos
   "Deletes all done todos."
   [state]
   (update state :todos
           (fn [todos]
-            (vec (remove :done todos)))))
+            (vec (remove :done? todos)))))
 
 (defn change-filter-mode
   "Changes the filter mode to the given keyword."
   [state filter-mode]
   (assoc state :filter-mode filter-mode))
+
+(defn change-draft-todo
+  "Changes the content of the currently editing todo."
+  [state draft-content]
+  (assoc state :draft-todo-content draft-content))
 
 ;; Queries.
 
@@ -61,21 +59,48 @@
   [{:keys [filter-mode todos]}]
   (case filter-mode
     :all todos
-    :active (remove :done todos)
-    :completed (filter :done todos)))
+    :active (remove :done? todos)
+    :completed (filter :done? todos)))
 
 ;; State.
 
 (defn initial-state []
   {:todos []
-   :filter-mode :all})
+   :filter-mode :all
+   :draft-todo-content ""})
 
 ;; Views.
+
+(defn todo-editor
+  "Allows you to create todos."
+  [state!]
+  (let [{:keys [draft-todo-content]} @state!]
+    [:input {:type "text"
+             :value draft-todo-content
+             :on-key-press #(when (= 13 (-> % .-charCode))
+                              (bonsai/next! state! create-todo))
+             :on-change #(bonsai/next! state! change-draft-todo (-> % .-target .-value))}]))
+
+(defn todo-list
+  "Renders the list of todos."
+  [state!]
+  (let [state @state!
+        todos (filtered-todos state)]
+    [:ul
+     (doall
+      (map-indexed
+       (fn [index {:keys [done? content]}]
+         [:li
+          {:key (str "todo-" index)}
+          (str done? " ") content])
+       todos))]))
 
 (defn root
   "The root component for the application, binds everything together."
   [state!]
-  [:p "..."])
+  [:div
+   [todo-editor state!]
+   [todo-list state!]])
 
 (defn init!
   "Entry point for the application, mounts the root into the DOM."
